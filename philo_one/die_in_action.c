cf65eb94acc83886b97590f35292b3f0c544b6db
pierrecariou <pierrecariou@outlook.fr>
2021-02-09 18:12:18 +0100

    philo_one no leaks

--- philo_one/die_in_action.c
+++ philo_one/die_in_action.c
@@ -6,7 +6,7 @@
 /*   By: pcariou <pcariou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 12:21:30 by pcariou           #+#    #+#             */
-/*   Updated: 2021/02/08 13:07:39 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/02/09 18:09:41 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	die_while_eating(struct timeval *tv, int id, t_options *opt)
 		usleep(opt->time_d);
 		pthread_mutex_lock(&opt->locks[opt->philo_n]);
 		gettimeofday(&tv[1], NULL);
-		printf("%ld %d died\n", (tv[1].tv_sec * 1000) +
-		(tv[1].tv_usec / 1000), id);
+		if (g_alive)
+			printf("%ld %d died\n", (tv[1].tv_sec * 1000) +
+			(tv[1].tv_usec / 1000), id);
 		g_alive = 0;
-		while (1)
-			usleep(1);
+		pthread_mutex_unlock(&opt->locks[opt->philo_n]);
 	}
 }
 
@@ -36,10 +36,10 @@ void	die_in_action(struct timeval *tv, int id, t_options *opt, int time_a)
 		((tv[0].tv_sec * 1000) + (tv[0].tv_usec / 1000))));
 		pthread_mutex_lock(&opt->locks[opt->philo_n]);
 		gettimeofday(&tv[1], NULL);
-		printf("%ld %d died\n", (tv[1].tv_sec * 1000) +
-		(tv[1].tv_usec / 1000), id);
+		if (g_alive)
+			printf("%ld %d died\n", (tv[1].tv_sec * 1000) +
+			(tv[1].tv_usec / 1000), id);
 		g_alive = 0;
-		while (1)
-			usleep(1);
+		pthread_mutex_unlock(&opt->locks[opt->philo_n]);
 	}
 }
--- philo_one/main.c
+++ philo_one/main.c
@@ -6,7 +6,7 @@
 /*   By: pcariou <pcariou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 14:18:37 by pcariou           #+#    #+#             */
-/*   Updated: 2021/02/08 12:03:03 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/02/09 18:09:48 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,9 @@ void	create_threads(t_options *opt)
 		return ;
 	while (++i < opt->philo_n)
 		pthread_create(&philo[i], NULL, philosopher, opt);
-	while (g_alive)
-		usleep(1);
+	i = -1;
+	while (++i < opt->philo_n)
+		pthread_join(philo[i], NULL);
 	free_all(opt, philo);
 }
 
